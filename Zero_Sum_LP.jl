737bbda6ec7e7e027a5c8116a9f7e4aace0677ec
LorenzoFiaschi <lorenzo.fiaschi@gmail.com>
2020-03-26 17:16:24 +0100

    Bug fixed

--- Zero_Sum_LP.jl
+++ Zero_Sum_LP.jl
@@ -9,15 +9,15 @@ function solve(A::Matrix{T},eps::Number=convert(promote_type(T,Float64),1e-5),ve
     _A = copy(A);
     
     # Make all the matrix entries non-negative
-    any(x->x<0, _A) && (_A = _A.+minimum(_A));
+    any(x->x<0, _A) && (_A = _A.-minimum(_A));
     
     n_row, n_col = size(_A);
    
     _b =  ones(T, n_row, 1);
     _c = -ones(T, n_col, 1);
-    _t =  ones(Float64, n_row);
+    _t =  ones(Int64, n_row);
     
-    obj, x, y, base = I_Big_M(_A, _b, _x, _t, eps, verbose, genLatex);
+    obj, x, y, base = I_Big_M(_A, _b, _c, _t, eps, verbose, genLatex);
     
     x /= sum(x);
     y /= sum(y);
---
+++ test/rock_paper_scissor.jl
@@ -0,0 +1,15 @@
+include("../Zero_Sum_LP.jl")
+include("../../ArithmeticNonStandarNumbersLibrary/BAN.jl")
+using .BAN
+
+A = zeros(Ban, 3, 3);
+
+A[1,2] =  one(Ban); A[1,3] = -one(Ban);
+A[2,1] = -one(Ban); A[2,3] =  one(Ban);
+A[3,1] =  one(Ban); A[3,2] = -one(Ban);
+
+x, y = solve(A);
+
+print("x: "); println(x);
+println("");
+print("y "); println(y);
