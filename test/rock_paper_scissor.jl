43e921dd70e04d177c6d97659821abfa802db6f8
LorenzoFiaschi <lorenzo.fiaschi@gmail.com>
2020-04-02 21:22:55 +0200

    Added test with infinitesimals

--- test/rock_paper_scissor.jl
+++ test/rock_paper_scissor.jl
@@ -4,9 +4,15 @@ using .BAN
 
 A = zeros(Ban, 3, 3);
 
+#=
 A[1,2] =  one(Ban); A[1,3] = -one(Ban);
 A[2,1] = -one(Ban); A[2,3] =  one(Ban);
 A[3,1] =  one(Ban); A[3,2] = -one(Ban);
+=#
+
+A[1,2] =  one(Ban) + 2*(one(Ban)>>1); A[1,3] = -one(Ban) - 2*(one(Ban)>>1);
+A[2,1] = -one(Ban) - 2*(one(Ban)>>1); A[2,3] =  one(Ban) + 2*(one(Ban)>>1);
+A[3,1] =  one(Ban) + 2*(one(Ban)>>1); A[3,2] = -one(Ban) - 2*(one(Ban)>>1);
 
 x, y = solve(A);
 
